47b5b768bd4842bb87c5985e0310f8eaef68f0e1
Lioteron <lioteronex@gmail.com>
2023-12-19 12:40:55 +0100

    New update.

--- app.js
+++ app.js
@@ -3,14 +3,16 @@ const cartaNavidad = document.querySelector(".carta-navidad");
 const containerInterior = document.querySelector(".container-interior");
 const titulo = containerInterior.firstElementChild;
 const subTitulo = document.querySelector(".subtitulo");
+const carruselImagenes = document.querySelector("#carousel-container");
 
 let indexAnimacion = 1;
 
-botonCerrar.addEventListener("click", function() {
+botonCerrar.addEventListener("click", function () {
     cartaNavidad.classList.add("animacion-carta");
     titulo.classList.add("animacion-carta");
     subTitulo.classList.add("animacion-carta");
     containerInterior.classList.add("animacion-carta");
+    carruselImagenes.classList.add("animacion-carta");
     console.log(subTitulo);
 
     switch (indexAnimacion) {
@@ -19,11 +21,32 @@ botonCerrar.addEventListener("click", function() {
             containerInterior.style.borderRadius = "0px";
             titulo.style.scale = "0";
             subTitulo.style.display = "block";
-            indexAnimacion++;
-            break;
-        case 2:
             subTitulo.style.scale = "1";
-            cartaNavidad.style.height = "600px";
-            i++;
+            cartaNavidad.style.height = "500px";
+            carruselImagenes.style.scale = "1";
+
+            setTimeout(function () {
+                carruselImagenes.style.opacity = "1";
+            }, 800); 
+
     }
 });
+
+let currentIndex = 0;
+const totalSlides = document.querySelectorAll('.slide').length;
+
+function showSlide(index) {
+    const slider = document.getElementById('image-slider');
+    const slideWidth = document.querySelector('.slide').clientWidth;
+    slider.style.transform = `translateX(${-index * slideWidth}px)`;
+}
+
+function nextSlide() {
+    currentIndex = (currentIndex + 1) % totalSlides;
+    showSlide(currentIndex);
+}
+
+function prevSlide() {
+    currentIndex = (currentIndex - 1 + totalSlides) % totalSlides;
+    showSlide(currentIndex);
+}
